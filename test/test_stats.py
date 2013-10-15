002a7cc0a1146a956f60b7791bfbb4e04eea358e
Dan Lecocq <dan@seomoz.org>
2013-10-15 14:43:44 -0700

    Added a 'worker' parameter to 'put'
    This was so that we could detect the case where a worker that owns a job is the one
    that's invoking the 'put'. In that case, we'd like to avoid sending the lock lost
    message since presumably it knows that it has moved the job.

--- test/test_stats.py
+++ test/test_stats.py
@@ -23,7 +23,7 @@ class TestStats(TestQless):
         # times to ensure that we know stats about how long they've waited
         jids = map(str, range(20))
         for jid in jids:
-            self.lua('put', 0, 'queue', jid, 'klass', {}, 0)
+            self.lua('put', 0, 'worker', 'queue', jid, 'klass', {}, 0)
         for jid in jids:
             self.lua('pop', jid, 'queue', 'worker', 1)
 
@@ -40,7 +40,7 @@ class TestStats(TestQless):
         # we'll complete them at different times and check the computed stats
         jids = map(str, range(20))
         for jid in jids:
-            self.lua('put', 0, 'queue', jid, 'klass', {}, 0)
+            self.lua('put', 0, 'worker', 'queue', jid, 'klass', {}, 0)
             self.lua('pop', 0, 'queue', 'worker', 1)
         for jid in jids:
             self.lua('complete', jid, jid, 'worker', 'queue', {})
@@ -57,7 +57,7 @@ class TestStats(TestQless):
         # The distinction here between 'failed' and 'failure' is that 'failed'
         # is the number of jobs that are currently failed, as opposed to the
         # number of times a job has failed in that queue
-        self.lua('put', 0, 'queue', 'jid', 'klass', {}, 0)
+        self.lua('put', 0, 'worker', 'queue', 'jid', 'klass', {}, 0)
         self.lua('pop', 0, 'queue', 'worker', 1)
         self.lua('fail', 0, 'jid', 'worker', 'group', 'message', {})
         stats = self.lua('stats', 0, 'queue', 0)
@@ -66,14 +66,14 @@ class TestStats(TestQless):
 
         # If we put the job back in a queue, we don't see any failed jobs,
         # but we still see a failure
-        self.lua('put', 0, 'queue', 'jid', 'klass', {}, 0)
+        self.lua('put', 0, 'worker', 'queue', 'jid', 'klass', {}, 0)
         stats = self.lua('stats', 0, 'queue', 0)
         self.assertEqual(stats['failed'], 0)
         self.assertEqual(stats['failures'], 1)
 
     def test_failed_cancel(self):
         '''If we fail a job, and then cancel it, stats reflects 0 failed job'''
-        self.lua('put', 0, 'queue', 'jid', 'klass', {}, 0)
+        self.lua('put', 0, 'worker', 'queue', 'jid', 'klass', {}, 0)
         self.lua('pop', 0, 'queue', 'worker', 1)
         self.lua('fail', 0, 'jid', 'worker', 'group', 'message', {})
         self.lua('cancel', 0, 'jid')
@@ -84,7 +84,7 @@ class TestStats(TestQless):
     def test_retries(self):
         '''It correctly tracks retries in a queue'''
         self.lua('config.set', 0, 'heartbeat', '-10')
-        self.lua('put', 0, 'queue', 'jid', 'klass', {}, 0)
+        self.lua('put', 0, 'worker', 'queue', 'jid', 'klass', {}, 0)
         self.lua('pop', 0, 'queue', 'worker', 1)
         self.assertEqual(self.lua('stats', 0, 'queue', 0)['retries'], 0)
         self.lua('pop', 0, 'queue', 'worker', 1)
@@ -92,17 +92,17 @@ class TestStats(TestQless):
 
     def test_original_day(self):
         '''It updates stats for the original day of stats'''
-        self.lua('put', 0, 'queue', 'jid', 'klass', {}, 0)
+        self.lua('put', 0, 'worker', 'queue', 'jid', 'klass', {}, 0)
         self.lua('pop', 0, 'queue', 'worker', 1)
         self.lua('fail', 0, 'jid', 'worker', 'group', 'message', {})
         # Put it somehwere 1.5 days later
-        self.lua('put', 129600, 'queue', 'jid', 'klass', {}, 0)
+        self.lua('put', 129600, 'worker', 'queue', 'jid', 'klass', {}, 0)
         self.assertEqual(self.lua('stats', 0, 'queue', 0)['failed'], 0)
         self.assertEqual(self.lua('stats', 0, 'queue', 129600)['failed'], 0)
 
     def test_failed_retries(self):
         '''It updates stats for jobs failed from retries'''
-        self.lua('put', 0, 'queue', 'jid', 'klass', {}, 0, 'retries', 0)
+        self.lua('put', 0, 'worker', 'queue', 'jid', 'klass', {}, 0, 'retries', 0)
         self.lua('pop', 1, 'queue', 'worker', 10)
         self.lua('retry', 3, 'jid', 'queue', 'worker')
         self.assertEqual(self.lua('stats', 0, 'queue', 0)['failed'], 1)
@@ -113,7 +113,7 @@ class TestStats(TestQless):
         '''Can cancel job that has been failed from retries through pop'''
         self.lua('config.set', 0, 'heartbeat', -10)
         self.lua('config.set', 0, 'grace-period', 0)
-        self.lua('put', 0, 'queue', 'jid', 'klass', {}, 0, 'retries', 0)
+        self.lua('put', 0, 'worker', 'queue', 'jid', 'klass', {}, 0, 'retries', 0)
         self.lua('pop', 1, 'queue', 'worker', 10)
         self.lua('pop', 2, 'queue', 'worker', 10)
         self.assertEqual(self.lua('stats', 0, 'queue', 0)['failed'], 1)
--- test/test_tag.py
+++ test/test_tag.py
@@ -25,39 +25,39 @@ class TestTag(TestQless):
 
     def test_add(self):
         '''Add a tag'''
-        self.lua('put', 0, 'queue', 'jid', 'klass', {}, 0)
+        self.lua('put', 0, 'worker', 'queue', 'jid', 'klass', {}, 0)
         self.lua('tag', 0, 'add', 'jid', 'foo')
         self.assertEqual(self.lua('get', 0, 'jid')['tags'], ['foo'])
 
     def test_remove(self):
         '''Remove a tag'''
-        self.lua('put', 0, 'queue', 'jid', 'klass', {}, 0, 'tags', ['foo'])
+        self.lua('put', 0, 'worker', 'queue', 'jid', 'klass', {}, 0, 'tags', ['foo'])
         self.lua('tag', 0, 'remove', 'jid', 'foo')
         self.assertEqual(self.lua('get', 0, 'jid')['tags'], {})
 
     def test_add_existing(self):
         '''We shouldn't double-add tags that already exist for the job'''
-        self.lua('put', 0, 'queue', 'jid', 'klass', {}, 0, 'tags', ['foo'])
+        self.lua('put', 0, 'worker', 'queue', 'jid', 'klass', {}, 0, 'tags', ['foo'])
         self.lua('tag', 0, 'add', 'jid', 'foo')
         self.assertEqual(self.lua('get', 0, 'jid')['tags'], ['foo'])
 
     def test_remove_nonexistent(self):
         '''Removing a nonexistent tag from a job is ok'''
-        self.lua('put', 0, 'queue', 'jid', 'klass', {}, 0, 'tags', ['foo'])
+        self.lua('put', 0, 'worker', 'queue', 'jid', 'klass', {}, 0, 'tags', ['foo'])
         self.lua('tag', 0, 'remove', 'jid', 'bar')
         self.assertEqual(self.lua('get', 0, 'jid')['tags'], ['foo'])
 
     def test_add_multiple(self):
         '''Adding the same tag twice at the same time yields no duplicates'''
-        self.lua('put', 0, 'queue', 'jid', 'klass', {}, 0)
+        self.lua('put', 0, 'worker', 'queue', 'jid', 'klass', {}, 0)
         self.lua('tag', 0, 'add', 'jid', 'foo', 'foo', 'foo')
         self.assertEqual(self.lua('get', 0, 'jid')['tags'], ['foo'])
 
     def test_get(self):
         '''Should be able to get jobs taggs with a particular tag'''
-        self.lua('put', 0, 'queue', 'foo', 'klass', {}, 0,
+        self.lua('put', 0, 'worker', 'queue', 'foo', 'klass', {}, 0,
             'tags', ['foo', 'both'])
-        self.lua('put', 0, 'queue', 'bar', 'klass', {}, 0,
+        self.lua('put', 0, 'worker', 'queue', 'bar', 'klass', {}, 0,
             'tags', ['bar', 'both'])
         self.assertEqual(
             self.lua('tag', 0, 'get', 'foo', 0, 10)['jobs'], ['foo'])
@@ -68,7 +68,7 @@ class TestTag(TestQless):
 
     def test_get_add(self):
         '''When adding a tag, it should be available for searching'''
-        self.lua('put', 0, 'queue', 'jid', 'klass', {}, 0)
+        self.lua('put', 0, 'worker', 'queue', 'jid', 'klass', {}, 0)
         self.assertEqual(
             self.lua('tag', 0, 'get', 'foo', 0, 10)['jobs'], {})
         self.lua('tag', 0, 'add', 'jid', 'foo')
@@ -77,7 +77,7 @@ class TestTag(TestQless):
 
     def test_order(self):
         '''It should preserve the order of the tags'''
-        self.lua('put', 0, 'queue', 'jid', 'klass', {}, 0)
+        self.lua('put', 0, 'worker', 'queue', 'jid', 'klass', {}, 0)
         tags = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
         for tag in tags:
             self.lua('tag', 0, 'add', 'jid', tag)
@@ -92,7 +92,7 @@ class TestTag(TestQless):
 
     def test_cancel(self):
         '''When a job is canceled, it's not found in tags'''
-        self.lua('put', 0, 'queue', 'jid', 'klass', {}, 0, 'tags', ['foo'])
+        self.lua('put', 0, 'worker', 'queue', 'jid', 'klass', {}, 0, 'tags', ['foo'])
         self.assertEqual(
             self.lua('tag', 0, 'get', 'foo', 0, 10)['jobs'], ['jid'])
         self.lua('cancel', 0, 'jid')
@@ -102,13 +102,13 @@ class TestTag(TestQless):
     def test_expired_jobs(self):
         '''When a job expires, it's removed from its tags'''
         self.lua('config.set', 0, 'jobs-history', 100)
-        self.lua('put', 0, 'queue', 'jid', 'klass', {}, 0, 'tags', ['foo'])
+        self.lua('put', 0, 'worker', 'queue', 'jid', 'klass', {}, 0, 'tags', ['foo'])
         self.lua('pop', 0, 'queue', 'worker', 10)
         self.lua('complete', 0, 'jid', 'worker', 'queue', {})
         self.assertEqual(
             self.lua('tag', 99, 'get', 'foo', 0, 10)['jobs'], ['jid'])
         # We now need another job to complete to expire this job
-        self.lua('put', 101, 'queue', 'foo', 'klass', {}, 0)
+        self.lua('put', 101, 'worker', 'queue', 'foo', 'klass', {}, 0)
         self.lua('pop', 101, 'queue', 'worker', 10)
         self.lua('complete', 101, 'foo', 'worker', 'queue', {})
         self.assertEqual(
@@ -117,13 +117,13 @@ class TestTag(TestQless):
     def test_expired_count_jobs(self):
         '''When a job expires from jobs-history-count, remove from its tags'''
         self.lua('config.set', 0, 'jobs-history-count', 1)
-        self.lua('put', 0, 'queue', 'jid', 'klass', {}, 0, 'tags', ['foo'])
+        self.lua('put', 0, 'worker', 'queue', 'jid', 'klass', {}, 0, 'tags', ['foo'])
         self.lua('pop', 0, 'queue', 'worker', 10)
         self.lua('complete', 0, 'jid', 'worker', 'queue', {})
         self.assertEqual(
             self.lua('tag', 0, 'get', 'foo', 0, 10)['jobs'], ['jid'])
         # We now need another job to complete to expire this job
-        self.lua('put', 1, 'queue', 'foo', 'klass', {}, 0)
+        self.lua('put', 1, 'worker', 'queue', 'foo', 'klass', {}, 0)
         self.lua('pop', 1, 'queue', 'worker', 10)
         self.lua('complete', 1, 'foo', 'worker', 'queue', {})
         self.assertEqual(
@@ -132,7 +132,7 @@ class TestTag(TestQless):
     def test_top(self):
         '''Ensure that we can find the most common tags'''
         for tag in range(10):
-            self.lua('put', 0, 'queue', tag, 'klass', {}, 0,
+            self.lua('put', 0, 'worker', 'queue', tag, 'klass', {}, 0,
                 'tags', range(tag, 10))
         self.assertEqual(self.lua('tag', 0, 'top', 0, 20),
             map(str, reversed(range(1, 10))))
@@ -149,7 +149,7 @@ class TestTag(TestQless):
         '''Pagination should work for tag.get'''
         jids = map(str, range(100))
         for jid in jids:
-            self.lua('put', jid, 'queue', jid, 'klass', {}, 0, 'tags', ['foo'])
+            self.lua('put', jid, 'worker', 'queue', jid, 'klass', {}, 0, 'tags', ['foo'])
         # Get two pages and ensure they're what we expect
         self.assertEqual(
             self.lua('tag', 100, 'get', 'foo',  0, 50)['jobs'], jids[:50])
@@ -161,7 +161,7 @@ class TestTag(TestQless):
         jids = map(str, range(10))
         for jid in jids:
             for suffix in map(str, range(int(jid) + 5)):
-                self.lua('put', jid, 'queue',
+                self.lua('put', jid, 'worker', 'queue',
                     jid + '.' + suffix, 'klass', {}, 0, 'tags', [jid])
         # Get two pages and ensure they're what we expect
         jids = list(reversed(jids))
