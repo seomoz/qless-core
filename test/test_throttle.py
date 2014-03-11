27aebd45e228a2a2dc13d8cfc64a51a835a05342
Danny Guinther <danny@backupify.com>
2014-03-11 15:31:48 -0400

    Add tests for dependent throttling

--- test/test_throttle.py
+++ test/test_throttle.py
@@ -139,4 +139,71 @@ class TestRelease(TestQless):
     self.assertEqual(self.lua('throttle.locks', 6, 'tid'), ['jid2'])
     self.assertEqual(self.lua('throttle.pending', 7, 'tid'), ['jid1'])
 
+class TestDependents(TestQless):
+  def test_dependencies_can_acquire_lock_after_dependent_success(self):
+    self.lua('throttle.set', 0, 'tid', 1)
+    self.lua('put', 1, 'worker', 'queue', 'jid1', 'klass', {}, 0, 'throttle', 'tid')
+    self.lua('put', 2, 'worker', 'queue', 'jid2', 'klass', {}, 0, 'depends', ['jid1'], 'throttle', 'tid')
+    self.lua('put', 3, 'worker', 'queue', 'jid3', 'klass', {}, 0, 'depends', ['jid2'], 'throttle', 'tid')
+
+    self.lua('pop', 4, 'queue', 'worker', 1)
+    self.assertEqual(self.lua('throttle.locks', 5, 'tid'), ['jid1'])
+    self.assertEqual(self.lua('throttle.pending', 6, 'tid'), [])
+    self.lua('complete', 7, 'jid1', 'worker', 'queue', {})
+
+    self.lua('pop', 0, 'queue', 'worker', 1)
+    self.assertEqual(self.lua('throttle.locks', 0, 'tid'), ['jid2'])
+    self.assertEqual(self.lua('throttle.pending', 0, 'tid'), [])
+    self.lua('complete', 0, 'jid2', 'worker', 'queue', {})
+
+    self.lua('pop', 0, 'queue', 'worker', 1)
+    self.assertEqual(self.lua('throttle.locks', 0, 'tid'), ['jid3'])
+    self.assertEqual(self.lua('throttle.pending', 0, 'tid'), [])
+    self.lua('complete', 0, 'jid3', 'worker', 'queue', {})
+
+    self.assertEqual(self.lua('throttle.locks', 0, 'tid'), [])
+    self.assertEqual(self.lua('throttle.pending', 0, 'tid'), [])
+
+  def test_dependencies_can_acquire_lock_after_dependent_failure(self):
+    self.lua('throttle.set', 0, 'tid', 1)
+    self.lua('put', 0, 'worker', 'queue', 'jid1', 'klass', {}, 0, 'throttle', 'tid')
+    self.lua('put', 0, 'worker', 'queue', 'jid2', 'klass', {}, 0, 'depends', ['jid1'], 'throttle', 'tid')
+    self.lua('put', 0, 'worker', 'queue', 'jid3', 'klass', {}, 0, 'depends', ['jid2'], 'throttle', 'tid')
+
+    self.lua('pop', 0, 'queue', 'worker', 1)
+    self.assertEqual(self.lua('throttle.locks', 0, 'tid'), ['jid1'])
+    self.assertEqual(self.lua('throttle.pending', 0, 'tid'), [])
+    self.lua('fail', 0, 'jid1', 'worker', 'failed', 'i failed', {})
+
+    self.assertEqual(self.lua('throttle.locks', 0, 'tid'), [])
+    self.assertEqual(self.lua('throttle.pending', 0, 'tid'), [])
+
+  def test_dependencies_do_not_acquire_lock_on_dependent_retry(self):
+    self.lua('throttle.set', 0, 'tid', 1)
+    self.lua('put', 1, 'worker', 'queue', 'jid1', 'klass', {}, 0, 'throttle', 'tid')
+    self.lua('put', 2, 'worker', 'queue', 'jid2', 'klass', {}, 0, 'depends', ['jid1'], 'throttle', 'tid')
+    self.lua('put', 3, 'worker', 'queue', 'jid3', 'klass', {}, 0, 'depends', ['jid2'], 'throttle', 'tid')
+
+    self.lua('pop', 0, 'queue', 'worker', 1)
+    self.assertEqual(self.lua('throttle.locks', 0, 'tid'), ['jid1'])
+    self.assertEqual(self.lua('throttle.pending', 0, 'tid'), [])
+    self.lua('retry', 0, 'jid1', 'queue', 'worker', 0, 'retry', 'retrying')
+
+    self.lua('pop', 0, 'queue', 'worker', 1)
+    self.assertEqual(self.lua('throttle.locks', 0, 'tid'), ['jid1'])
+    self.assertEqual(self.lua('throttle.pending', 0, 'tid'), [])
+    self.lua('complete', 0, 'jid1', 'worker', 'queue', {})
+
+    self.lua('pop', 0, 'queue', 'worker', 1)
+    self.assertEqual(self.lua('throttle.locks', 0, 'tid'), ['jid2'])
+    self.assertEqual(self.lua('throttle.pending', 0, 'tid'), [])
+    self.lua('complete', 0, 'jid2', 'worker', 'queue', {})
+
+    self.lua('pop', 0, 'queue', 'worker', 1)
+    self.assertEqual(self.lua('throttle.locks', 0, 'tid'), ['jid3'])
+    self.assertEqual(self.lua('throttle.pending', 0, 'tid'), [])
+    self.lua('complete', 0, 'jid3', 'worker', 'queue', {})
+    self.assertEqual(self.lua('throttle.locks', 0, 'tid'), [])
+    self.assertEqual(self.lua('throttle.pending', 0, 'tid'), [])
+
 # class TestRecurring(TestQless):
